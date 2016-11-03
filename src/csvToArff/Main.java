4fe892705b12330d94f3ec8e91ae45ec9daf01d0
Ross <ross@Rosss-MacBook.local>
2016-11-03 17:11:30 +0000

    linked hash map generates with the headers as the keys and the information as the values

--- src/csvToArff/Main.java
+++ src/csvToArff/Main.java
@@ -2,20 +2,22 @@ package csvToArff;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class Main {
 
 	
 	public static ArrayList<ArrayList<String>> csv = new ArrayList<ArrayList<String>>();
 	
-	public static HashMap<String, ArrayList<String>> attributes = new HashMap<String,ArrayList<String>>();
+	public static LinkedHashMap<String, ArrayList<String>> attributes = new LinkedHashMap<String,ArrayList<String>>();
 	
 	public static void main(String args[]){
 		
-		CSVParser parser = new CSVParser(args[0],",", false);
+		CSVParser parser = new CSVParser("/Users/ross/Downloads/uk-500.csv",",", false);
 		parser.clearEmpties();
 		csv = parser.getCSV();
-		
+		setAttributeKeys();
+		System.out.println(attributes);
 		
 	}
 	
@@ -23,11 +25,23 @@ public class Main {
 	
 	public static void setAttributeKeys(){
 		ArrayList<String> header = csv.get(0);
+		csv.remove(0); //Stops the headers being included in the values list
 		
 		
-		for(String cursor : header){
-			attributes.put(cursor, new ArrayList<String>());
+		for(int i = 0; i < header.size(); i++){
+			ArrayList<String> values = new ArrayList<String>();
+			for(ArrayList<String> line : csv){
+				values.add(line.get(i));
+			}
+			attributes.put(header.get(i), values);
 		}
+		
+		
+		
+		 
+		
+		
+		
 	}
 	
 	
